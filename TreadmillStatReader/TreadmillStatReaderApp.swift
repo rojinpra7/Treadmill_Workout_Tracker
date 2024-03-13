eaf4a4a09ccc969af0297b99a04b58ecda5492a0
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2024-03-13 16:08:29 -0400

    Initial Commit

---
+++ TreadmillStatReader/TreadmillStatReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreadmillStatReaderApp.swift
+//  TreadmillStatReader
+//
+//  Created by Rojin Prajapati on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TreadmillStatReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
