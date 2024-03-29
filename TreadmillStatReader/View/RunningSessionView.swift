6bf1f92e7f59eab086012e9c7cd0a18768a8318a
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2024-03-29 01:13:47 -0400

    Create a RunningSessionView

--- TreadmillStatReader/View/RunningSessionView.swift
+++ TreadmillStatReader/View/RunningSessionView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct RunningSessionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map{
+            
+        }
     }
 }
 
